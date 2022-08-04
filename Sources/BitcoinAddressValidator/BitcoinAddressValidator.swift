ab7a83c531f09486f99afeb16bbacad77ce254c0
cuhte3 <farid@getportal.co>
2022-08-04 17:09:16 +0700

    public init added

--- Sources/BitcoinAddressValidator/BitcoinAddressValidator.swift
+++ Sources/BitcoinAddressValidator/BitcoinAddressValidator.swift
@@ -3,6 +3,8 @@ import Foundation
 public struct BitcoinAddressValidator {
     private let validators: [IAddressValidator] = [Base58Validator(), SegWitValidator()]
     
+    public init() {}
+    
     public func isValid(address: String) -> Bool {
         validators.first(where: { $0.isValid(address: address )}) != nil
     }
